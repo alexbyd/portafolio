187ac8771659aa759268bd4b8ed54587982f6b8b
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2022-10-08 16:09:00 -0500

    agregue el texto despcriptivo aunque esta en español con sus enlaces

--- src/App.js
+++ src/App.js
@@ -24,8 +24,6 @@ function App() {
       titulo='Sobre mi'
   />
 </div>
-
-
     </div>
   );
 }
--- src/components/Descripcion.js
+++ src/components/Descripcion.js
@@ -8,13 +8,24 @@ function Descripcion(props) {
   return(
       <div className='lista'>
         <h4 className='subtitulo'>{props.titulo}</h4>
+        <p>Me considero autodidacta y con ganas de nunca dejar de aprender y
+          practicar para hacer eficiente lo aprendido, creo que la curiosidad
+          por su naturaleza esta unido a nosotros los humanos, me gusta la
+          fotografia y se tocar guitarra.
+          Trato de resolver problemas de manera sistematica, en la practica vi que
+          muchos problemas tienen la misma forma creando asi patrones.
+          trabaje en&nbsp;
+          <a href={("https://cookie-monster.vercel.app")} className="color-verde">Monstruo de las galletas,&nbsp;&nbsp;</a>
+          <a href={("https://somedclin.vercel.app")} className="color-verde">Somedclin,&nbsp;</a>
+          <a href={("https://alexbaena.me/")} className="color-verde">&nbsp;Portafolio</a>.</p>
         <div className='lista-detalles'>
         <ul>
-          <li>creador de el monstruo de las galletas</li>
-          <li>investigar y comprender como funciona fetch y hacer una entrada de
-          blog con eso para mostrar</li>
-          <li>crear una funcion en js que cree 'li' de manera iterativa y se pasa al
-          componente mediante un props</li>
+          <li> Actualmente estoy trabajando en...  {/* este es el color para los enlaces  --> #1C9963 */}</li>
+          <li> Actualmente estoy aprendiendo... como funciona fetch </li>
+          <li> Busco colaborar en...FreeCodeCamp mediante hackoctoberfest</li>
+          <li> Pregúntame sobre...</li>
+          <li> Cómo contactarme: ...</li>
+          <li> Dato curioso: ...</li>
         </ul>
       </div>
 
