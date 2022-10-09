c208a68733781098720597de30430fa6f0cc9eae
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2022-10-08 20:01:34 -0500

    cambiel el margin y padding de los elemenatos, lo proximo es organizarlo con variables y calc css para que quede estandarizado

--- src/components/Descripcion.js
+++ src/components/Descripcion.js
@@ -8,9 +8,9 @@ function Descripcion(props) {
   return(
       <div className='lista'>
         <h4 className='subtitulo'>{props.titulo}</h4>
-        <p>Me considero autodidacta y con ganas de nunca dejar de aprender y
+        <p>Me considero autodidacta  con ganas de nunca dejar de aprender y
           practicar para hacer eficiente lo aprendido, creo que la curiosidad
-          por su naturaleza esta unido a nosotros los humanos, me gusta la
+          por su naturaleza esta unida a nosotros los humanos, me gusta la
           fotografia y se tocar guitarra.
           Trato de resolver problemas de manera sistematica, en la practica vi que
           muchos problemas tienen la misma forma creando asi patrones.
@@ -20,12 +20,12 @@ function Descripcion(props) {
           <a href={("https://alexbaena.me/")} className="color-verde">&nbsp;Portafolio</a>.</p>
         <div className='lista-detalles'>
         <ul>
-          <li> Actualmente estoy trabajando en...  {/* este es el color para los enlaces  --> #1C9963 */}</li>
-          <li> Actualmente estoy aprendiendo... como funciona fetch </li>
-          <li> Busco colaborar en...FreeCodeCamp mediante hackoctoberfest</li>
-          <li> Pregúntame sobre...</li>
-          <li> Cómo contactarme: ...</li>
-          <li> Dato curioso: ...</li>
+          <li>- Actualmente estoy trabajando en...  {/* este es el color para los enlaces  --> #1C9963 */}</li>
+          <li>- Actualmente estoy aprendiendo... como funciona fetch </li>
+          <li>- Busco colaborar en...FreeCodeCamp mediante Hacktoberfest</li>
+          <li>- Pregúntame sobre...</li>
+          <li>- Cómo contactarme: ...</li>
+          <li>- Dato curioso: ...</li>
         </ul>
       </div>
 
