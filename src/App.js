b1445efff601d4cc5347ac7c1ce79f3dc17f1205
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2022-12-27 23:57:07 -0500

    Restaure los archivos pero no es el commit donde borre npm start

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import './small.css';
+import Descripcion from "./components/Descripcion";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav>
+        <div className='seguir'>
+          <h1>Alex Daniel Baena Yepez</h1>
+        </div>
+      </nav>
+
+<div className='division'>
+      <div className='nombre-principal'>
+        <div className='nombre'>
+          <h2>@alexbyd{/* <!--poner aqui los iconos para el tamaño small --> */}</h2>
+        </div>
+      </div>
+
+  <Descripcion
+      titulo='Sobre mi'
+  />
+</div>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Descripcion.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../styles/descripcion.css';
+import '../App.css';
+import '../small.css';
+
+
+function Descripcion(props) {
+  return(
+      <div className='lista'>
+        <h4 className='subtitulo'>{props.titulo}</h4>
+        <div className='lista-detalles'>
+        <ul>
+          <li>creador de el monstruo de las galletas</li>
+          <li>investigar y comprender como funciona fetch y hacer una entrada de
+          blog con eso para mostrar</li>
+          <li>crear una funcion en js que cree 'li' de manera iterativa y se pasa al
+          componente mediante un props</li>
+        </ul>
+      </div>
+
+
+
+        <div className='contenedor-foto'>
+          <div className='paraLaFoto'>
+            <img className='foto-alex'
+                 src={require('../images/alexD-r1.png')}
+                 alt='alex baena'
+            />
+          </div>
+        </div>
+
+      </div>
+  );
+}
+
+export default Descripcion;
